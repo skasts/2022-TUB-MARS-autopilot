f761f0ed508fa52c31d49a9746e81cf1fa80d940
Simon Kast <57326251+skasts@users.noreply.github.com>
2022-06-30 15:15:24 +0000

    First launchable version

--- README.md
+++ README.md
@@ -13,10 +13,15 @@ sudo usermod -aG docker $USER
 
 ### Commands
 colcon build --symlink-install
+ros2 daemon stop; ros2 daemon start
+
 . install/local_setup.bash
 ros2 run virtual_maize_field generate_world fre22_task_navigation_mini --show_map
 pkill gzserver
 ros2 launch virtual_maize_field simulation.launch.py headless:='True' verbose:='True'
 ros2 launch jackal_custom robot_state_publisher.launch.py
 ros2 launch jackal_custom spawn_robot.launch.py
+ros2 launch autopilot autopilot.launch.py
 ros2 launch gazebo_ros gzclient.launch.py use_sim_time:='True' verbose:='True'
+rviz2 -d src/autopilot/config/default_view.rviz
+ros2 service call /delete_entity gazebo_msgs/srv/DeleteEntity "{name: 'robot'}"
--- src/jackal_custom/launch/robot_state_publisher.launch.py
+++ src/jackal_custom/launch/robot_state_publisher.launch.py
@@ -3,8 +3,9 @@ import launch
 
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, Command
 from launch_ros.actions import Node
+from rcl_interfaces.msg import ParameterValue
 
 def generate_launch_description():
     # Create launch description object
@@ -13,20 +14,35 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration("use_sim_time", default="true")
     declare_use_sim_time_cmd = DeclareLaunchArgument(name="use_sim_time", default_value="True")
     
-    urdf = os.path.join(
-        get_package_share_directory("jackal_custom"), "urdf/jackal.urdf"
-    )
+    urdf = os.path.join(get_package_share_directory("jackal_custom"), "urdf/jackal.urdf")
+    with open(urdf, 'r') as infp:
+        robot_desc = infp.read()
 
     state_publisher_cmd = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
-        parameters=[{'use_sim_time': use_sim_time}],
-        arguments=[urdf],
+        parameters=[{'use_sim_time': True, 
+                     'robot_description': robot_desc}]
+        # arguments=[urdf],
     )
 
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(state_publisher_cmd)
 
     return ld
+
+    # For urdf.xacro:
+    # urdf = os.path.join(get_package_share_directory("jackal_custom"), "urdf/jackal.urdf.xacro")
+    # state_publisher_cmd = Node(
+    #     package='robot_state_publisher',
+    #     executable='robot_state_publisher',
+    #     name='robot_state_publisher',
+    #     output='screen',
+    #     parameters=[{'use_sim_time': use_sim_time, 
+    #                  'robot_description': ParameterValue(
+    #             Command(['xacro ', str(urdf)]), value_type=str
+    #         )}]
+    #     # arguments=[urdf],
+    # )
