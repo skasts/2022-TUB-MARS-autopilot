0f1ecec9ce4bc5094be851630b9e44fcd0960cf4
Simon Kast <57326251+skasts@users.noreply.github.com>
2022-07-12 20:37:05 +0000

    Update to mars_robot

--- src/mars_robot/launch/diff_drive_controller.launch.py
+++ src/mars_robot/launch/diff_drive_controller.launch.py
@@ -3,6 +3,7 @@ import launch
 
 from ament_index_python.packages import get_package_share_directory
 from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch.substitutions import LaunchConfiguration, Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -11,6 +12,9 @@ def generate_launch_description():
     # Create launch description object
     ld = launch.LaunchDescription()
 
+    # Argument, if to start RViz with robot view
+    arg_show_rviz = DeclareLaunchArgument("start_rviz",default_value="false")
+
     # Get URDF via xacro
     robot_description_content = Command(
         [
@@ -18,25 +22,27 @@ def generate_launch_description():
             " ",
             PathJoinSubstitution(
                 [FindPackageShare("mars_robot"), "urdf", "mars_robot.urdf.xacro"]
+                # [FindPackageShare("diffbot_description"), "urdf", "diffbot_system.urdf.xacro"]
             ),
         ]
     )
     robot_description = {"robot_description": robot_description_content}
 
+    diff_drive_controller = os.path.join(get_package_share_directory("mars_robot"),"config/diff_drive_controller.yaml")
+
     robot_state_pub_cmd = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        output="both",
+        output="screen",
         parameters=[robot_description],
     )
 
-    diffbot_diff_drive_controller = os.path.join(get_package_share_directory("mars_robot"),"config/diff_drive_controller.yaml")
     controller_manager_cmd = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description, diffbot_diff_drive_controller],
+        parameters=[robot_description, diff_drive_controller],
         remappings=[
-            ("/diffbot_base_controller/cmd_vel_unstamped", "/cmd_vel"),
+            ("/base_controller/cmd_vel_unstamped", "/cmd_vel"),
         ],
         output={
             "stdout": "screen",
@@ -47,7 +53,7 @@ def generate_launch_description():
     spawn_dd_controller = Node(
         package="controller_manager",
         executable="spawner.py",
-        arguments=["diffbot_base_controller"],
+        arguments=["base_controller"],
         output="screen",
     )
 
@@ -57,21 +63,30 @@ def generate_launch_description():
         arguments=["joint_state_broadcaster"],
         output="screen",
     )
-    
-    ld.add_action(controller_manager_cmd)
-    ld.add_action(robot_state_pub_cmd)
-    ld.add_action(spawn_jsb_controller)
-    ld.add_action(spawn_dd_controller)
-    
-    return ld
 
-    # TODO: # Get URDF via xacro
-    # robot_description_content = Command(
-    #     [
-    #         PathJoinSubstitution([FindExecutable(name="xacro")]),
-    #         " ",
-    #         PathJoinSubstitution(
-    #             [FindPackageShare("diffbot_description"), "urdf", "diffbot_system.urdf.xacro"]
-    #         ),
-    #     ]
+    rviz_config_file = PathJoinSubstitution(
+        [FindPackageShare("mars_robot"), "config", "robot_view.rviz"]
+    )
+
+    rviz_cmd = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", rviz_config_file],
+        condition=IfCondition(LaunchConfiguration("start_rviz")),
+    )
+
+    # Timed only with humble?
+    # rviz_cmd_timed = TimerAction(
+    #     period = 3.0,
+    #     actions=[rviz_cmd]
     # )
+    
+    ld.add_action(robot_state_pub_cmd)
+    ld.add_action(controller_manager_cmd)
+    ld.add_action(spawn_dd_controller)
+    ld.add_action(spawn_jsb_controller)
+    ld.add_action(arg_show_rviz)
+    ld.add_action(rviz_cmd)
+
+    return ld
--- src/mars_robot/launch/robot_state_publisher.launch.py
+++ src/mars_robot/launch/robot_state_publisher.launch.py
@@ -1,7 +1,5 @@
-import os
 import launch
 
-from ament_index_python.packages import get_package_share_directory
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration, Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
@@ -14,32 +12,28 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration("use_sim_time", default="true")
     declare_use_sim_time_cmd = DeclareLaunchArgument(name="use_sim_time", default_value="True")
     
-    urdf = os.path.join(get_package_share_directory("mars_robot"), "urdf/mars_robot.urdf")
-    with open(urdf, 'r') as infp:
-        robot_desc = infp.read()
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [FindPackageShare("mars_robot"), "urdf", "mars_robot.urdf.xacro"]
+                # [FindPackageShare("diffbot_description"), "urdf", "diffbot_system.urdf.xacro"]
+            ),
+        ]
+    )
 
     state_publisher_cmd = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         name='robot_state_publisher',
         output='screen',
-        parameters=[{'use_sim_time': True, 
-                     'robot_description': robot_desc}]
-        # arguments=[urdf],
+        parameters=[{'use_sim_time': use_sim_time, 
+                     'robot_description': robot_description_content}]
     )
 
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(state_publisher_cmd)
 
     return ld
-
-    # TODO: # Get URDF via xacro
-    # robot_description_content = Command(
-    #     [
-    #         PathJoinSubstitution([FindExecutable(name="xacro")]),
-    #         " ",
-    #         PathJoinSubstitution(
-    #             [FindPackageShare("diffbot_description"), "urdf", "diffbot_system.urdf.xacro"]
-    #         ),
-    #     ]
-    # )
